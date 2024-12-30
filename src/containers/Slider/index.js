e67bce5831d746d56e17c53df0e55043a1069319
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-12-29 20:24:59 -0500

    Correction key props

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -25,13 +25,11 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
-            }`}
-          >
+            }`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -41,6 +39,7 @@ const Slider = () => {
               </div>
             </div>
           </div>
+            ))}
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((radio, radioIdx) => (
@@ -53,8 +52,6 @@ const Slider = () => {
               ))}
             </div>
           </div>
-        </>
-      ))}
     </div>
   );
 };
